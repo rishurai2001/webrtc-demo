69bb380f49e54c611de384d66d2f0d474b6d40cf
Rishu <rishurai2001@gmail.com>
2021-07-13 01:50:13 +0530

    done

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -40,9 +40,9 @@ export default function Register(props) {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">Lamasocial</h3>
+          <h3 className="loginLogo">Video-engage-app</h3>
           <span className="loginDesc">
-            Connect with friends and the world around you on Lamasocial.
+            Connect with friends and the world around you
           </span>
         </div>
         <div className="loginRight">
